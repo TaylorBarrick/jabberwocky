2a949d6e19e1f4e2b3a48a805607e3d663b8f026
Jacen <jacen.nicely@gmail.com>
2017-03-13 00:15:05 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+
+require("electron-reload")(__dirname);
+
+let mainWindow;
+
+app.on('ready', () => {
+    let win = new BrowserWindow({width:800, height:600})
+    win.loadURL(`file://${__dirname}/index.html`)
+})
+
+exports.openWindow = (filename) => {
+    let win = new BrowserWindow({width:800, height:600})
+    win.loadURL(`file://${__dirname}/` + filename + `.html`)    
+}
